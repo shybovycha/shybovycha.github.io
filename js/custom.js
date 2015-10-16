3fb5340ca42f4df816064b7af22ce52fe746c6f3
Artem Szubowicz <shybovycha@gmail.com>
2015-10-16 18:03:26 +0200

    Removed timer, replacing it with progress-bar. Started clojure post

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(function () {
-    $("#next-post-countdown").countdown("2015/10/14 22:00:00", function(event) {
-        $(this).text(event.strftime('%D days %H:%M:%S'));
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
     });
 
     $('.carousel').carousel({
